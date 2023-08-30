c7bc623101432ebf1e5761332798f4efb43da932
L0P3ZS <yoniersebastian23@gmail.com>
2023-08-30 12:38:24 -0500

    re

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -10,45 +10,69 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.interfaceService.IUsuarioService;
 import com.example.demo.modelo.Usuario;
 
 
-
+@RestController
 @Controller
-@RequestMapping
+@RequestMapping("/usuarios")
 public class Controlador {
 	
 	@Autowired
 	private IUsuarioService service;
 	
-	@GetMapping("/listar")
-	public String listar(Model model) {
-		List<Usuario>usuarios = service.listar();
-		model.addAttribute("usuarios", usuarios);
-		return "index";
+	@GetMapping()
+	@ResponseBody
+	public List<Usuario> listar(Model model) {
+		return service.listar();
 	}
 	
-	
-	@GetMapping("/new")
-	public String agregar( Model model) {
-		model.addAttribute("usuario",new Usuario());
-		return "form";
-	}
-	
-	@PostMapping("/regist")
+	/*
+	@PostMapping()
 	public String save(@Validated Usuario u,  Model model) {
 		service.save(u);
-		return "redirect:/listar";
+		return null;
 	}
+	*/
+	/*
+	   @PostMapping("/usuarios/save")
+	    public String save(@RequestParam("name") String name, 
+	                       @RequestParam("email") String email,
+	                       @RequestParam("username") String username,
+	                       @RequestParam("password") String password,
+	                       @RequestParam("lastname") String lastname) {
+	      
+	        Usuario u = new Usuario();
+	        u.setName(name);
+	        u.setEmail(email);
+	        u.setUsername(username);
+	        u.setPassword(password);
+	        u.setLastname(lastname);
+	   
+	        
+	        service.save(u); 
+	        
+	        return "redirect:/usuarios";
+	    }*/
+	   
+	 @PostMapping()
+	    public String save(@Validated @RequestBody Usuario u) {
+	        service.save(u); // Guardar el usuario
+	        return "Guardado exitosamente";
+	    }
 	
 	@GetMapping("/update/{id}")
 	public String update(@PathVariable int id,  Model model) {
 		Optional<Usuario>usuario = service.listarId(id);
 		model.addAttribute("usuario", usuario);
-		return "form";
+		return "redirect:/usuarios";
 	}
 	
 	@GetMapping("/delete/{id}")
--- src/main/java/com/example/demo/modelo/Usuario.java
+++ src/main/java/com/example/demo/modelo/Usuario.java
@@ -6,31 +6,26 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="usuarios")
+@Table(name="masc")
 public class Usuario {
 
 	 @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 		private int id;
 	    
-		private String name;
-		private String lastname;
-		private String username;
-		private String password;
-		private String email;
+		private String nombre;
+		private String raza;
 		
 		public Usuario() {
 			
 		}
 
-		public Usuario(int id, String name, String lastname, String username, String password, String email) {
+		public Usuario(int id, String nombre, String raza) {
 			super();
 			this.id = id;
-			this.name = name;
-			this.lastname = lastname;
-			this.username = username;
-			this.password = password;
-			this.email = email;
+			this.nombre = nombre;
+			this.raza = raza;
+			
 		}
 
 		public int getId() {
@@ -41,44 +36,22 @@ public class Usuario {
 			this.id = id;
 		}
 
-		public String getName() {
-			return name;
+		public String getNombre() {
+			return nombre;
 		}
 
-		public void setName(String name) {
-			this.name = name;
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
 		}
 
-		public String getLastname() {
-			return lastname;
+		public String getRaza() {
+			return raza;
 		}
 
-		public void setLastname(String lastname) {
-			this.lastname = lastname;
+		public void setRaza(String raza) {
+			this.raza = raza;
 		}
 
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public String getEmail() {
-			return email;
-		}
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
+		
 		
 }
